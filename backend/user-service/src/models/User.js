31d56f6152364157e82250a1f33eee5b55f968d4
Shadhir Fawz <it22237118@my.sliit.lk>
2026-03-13 06:01:37 +0530

    feat: models for user and membership

---
+++ backend/user-service/src/models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+{
+  fullName: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  phone: String,
+
+  role: {
+    type: String,
+    enum: ["ADMIN", "LIBRARIAN", "MEMBER"],
+    default: "MEMBER"
+  },
+
+  status: {
+    type: String,
+    enum: ["ACTIVE", "SUSPENDED"],
+    default: "ACTIVE"
+  },
+
+  membershipId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Membership"
+  },
+
+  address: {
+    street: String,
+    city: String,
+    district: String,
+    postalCode: String
+  },
+
+  profileImage: String,
+
+  lastLogin: Date
+},
+{
+  timestamps: true
+}
+);
+
+module.exports = mongoose.model("User", userSchema);
