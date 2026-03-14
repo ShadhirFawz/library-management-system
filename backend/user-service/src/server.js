8d2290971d696546cf89375c65a67ef01280b7f5
Shadhir Fawz <it22237118@my.sliit.lk>
2026-03-14 09:44:03 +0530

    feat: routes for authentication api

--- backend/user-service/src/server.js
+++ backend/user-service/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
 
 const connectDB = require("./config/db");
 
@@ -17,6 +18,8 @@ app.get("/health", (req, res) => {
     status: "running"
   });
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
 
